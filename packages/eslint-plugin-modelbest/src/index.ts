dcbb67863cd47c27778c8fc1d22e1b4184a31d6f
lovangry <272613008@qq.com>
2023-07-11 14:17:39 +0800

    feat: update

--- packages/eslint-plugin-modelbest/src/index.ts
+++ packages/eslint-plugin-modelbest/src/index.ts
@@ -2,7 +2,6 @@ import genericSpacing from './rules/generic-spacing'
 import ifNewline from './rules/if-newline'
 import importDedupe from './rules/import-dedupe'
 import preferInlineTypeImport from './rules/prefer-inline-type-import'
-import topLevelFunction from './rules/top-level-function'
 import noTsExportEqual from './rules/no-ts-export-equal'
 import noCjsExports from './rules/no-cjs-exports'
 import noConstEnum from './rules/no-const-enum'
@@ -13,7 +12,6 @@ export default {
     'import-dedupe': importDedupe,
     'prefer-inline-type-import': preferInlineTypeImport,
     'generic-spacing': genericSpacing,
-    'top-level-function': topLevelFunction,
     'no-cjs-exports': noCjsExports,
     'no-ts-export-equal': noTsExportEqual,
     'no-const-enum': noConstEnum,
